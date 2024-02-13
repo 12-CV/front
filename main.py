0370c036152f713b6d28bd2a9ca0ceba77e3b3bc
jae-heo <gjwodud119@gmail.com>
2024-02-13 12:50:38 +0900

    Add: PYQT5 스켈레톤 코드 초기화
    #1

---
+++ main.py
@@ -0,0 +1,23 @@
+import argparse
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+class MainApp(QMainWindow):
+    def __init__(self, mode):
+        super().__init__()
+        loadUi("ui/example.ui", self)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='test parser')
+    parser.add_argument('--example', type=str, help='example', default="nothing")
+    args = parser.parse_args()
+
+    app = QApplication(sys.argv)
+    main_window = MainApp(args)
+    main_window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
