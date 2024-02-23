ffddab710426636675dd8e18f69b2d0fc3b2db1d
jae-heo <gjwodud119@gmail.com>
2024-02-23 22:09:06 +0900

    Update: 파일 불러오기 코드 수정. 일단 mp4만 허용하도록 수정
    #6

--- main.py
+++ main.py
@@ -22,22 +22,20 @@ class MainApp(CMainWindow):
         self.plainTextEdit_mainLogger.setPlainText("[System] 영상을 입력하세요. ")
 
     def import_video_button_clicked(self):
-        self.file_name, _ = QFileDialog.getOpenFileName(self, "영상 불러오기", "", "Video Files (*.mp4 *.avi *.mov *.mkv);;All Files (*)")
-        if self.file_name:
+        # file_name, _ = QFileDialog.getOpenFileName(self, "영상 불러오기", "", "Video Files (*.mp4 *.avi *.mov *.mkv);;All Files (*)")
+        file_name, _ = QFileDialog.getOpenFileName(self, "영상 불러오기", "", "Video Files (*.mp4 *.avi)")
+        if file_name:
+            self.file_name = file_name
+
             cap = cv2.VideoCapture(self.file_name)
             self.fps = int(cap.get(cv2.CAP_PROP_FPS))
             self.video_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
             self.video_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            self.view.viewer_width = self.video_width
-            self.view.viewer_height = self.video_height
-
             video_info_text = f"\n파일 이름 : \n{self.file_name} \n\nfps : {self.fps} \n\nvideo size : {self.video_width} x {self.video_height}"
             self.plainTextEdit_videoInfo.setPlainText(video_info_text)
-            
-            self.view.video_play(self.file_name)
-            
         else:
-            show_message(self, "파일 불러오기에 실패했습니다.")
+            show_message(self, "파일이 존재하지 않습니다.")
+            return
 
 def main():
     parser = argparse.ArgumentParser(description='Program Mode')
